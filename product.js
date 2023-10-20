28333ed2f141128b9d9e961b175a33f5fb73d0a6
PARTH RASTOGI <84573632+Parthcode1005@users.noreply.github.com>
2023-10-20 12:45:22 +0530

    initial commit

---
+++ product.js
@@ -0,0 +1,606 @@
+const catalogue = [
+    {
+        "id": 1,
+        "imageURL": "./icons/black-polo-men.png",
+        "name": "Black Polo",
+        "type": "Polo",
+        "price": 250,
+        "currency": "INR",
+        "color": "Black",
+        "gender": "Men",
+        "quantity": 3
+    },
+    {
+        "id": 2,
+        "imageURL": "./icons/blue-polo-women.png",
+        "name": "Blue Polo",
+        "type": "Polo",
+        "price": 350,
+        "currency": "INR",
+        "color": "Blue",
+        "gender": "Women",
+        "quantity": 3
+    },
+    {
+        "id": 3,
+        "imageURL": "./icons/pink-polo-women.png",
+        "name": "Pink Polo",
+        "type": "Polo",
+        "price": 350,
+        "currency": "INR",
+        "color": "Pink",
+        "gender": "Women",
+        "quantity": 6
+    },
+    {
+        "id": 4,
+        "imageURL": "./icons/black-hoodie-men.png",
+        "name": "Black Hoodie",
+        "type": "Hoodie",
+        "price": 500,
+        "currency": "INR",
+        "color": "Black",
+        "gender": "Men",
+        "quantity": 2
+    },
+    {
+        "id": 5,
+        "imageURL": "./icons/green-polo-men.png",
+        "name": "Green Polo",
+        "type": "Polo",
+        "price": 250,
+        "currency": "INR",
+        "color": "Green",
+        "gender": "Men",
+        "quantity": 1
+    },
+    {
+        "id": 6,
+        "imageURL": "./icons/green-polo-women.png",
+        "name": "Green Polo",
+        "type": "Polo",
+        "price": 350,
+        "currency": "INR",
+        "color": "Green",
+        "gender": "Women",
+        "quantity": 1
+    },
+    {
+        "id": 7,
+        "imageURL": "./icons/blue-hoodie-women.png",
+        "name": "Blue Hoodie",
+        "type": "Hoodie",
+        "price": 500,
+        "currency": "INR",
+        "color": "Blue",
+        "gender": "Women",
+        "quantity": 2
+    },
+    {
+        "id": 8,
+        "imageURL": "./icons/black-hoodie-women.png",
+        "name": "Black Hoodie",
+        "type": "Hoodie",
+        "price": 500,
+        "currency": "INR",
+        "color": "Black",
+        "gender": "Women",
+        "quantity": 5
+    },
+    {
+        "id": 9,
+        "imageURL": "./icons/blue-round-men.png",
+        "name": "Blue Round",
+        "type": "Basic",
+        "price": 300,
+        "currency": "INR",
+        "color": "Blue",
+        "gender": "Men",
+        "quantity": 2
+    },
+    {
+        "id": 10,
+        "imageURL": "./icons/red-round-women.png",
+        "name": "Red Round",
+        "type": "Basic",
+        "price": 300,
+        "currency": "INR",
+        "color": "Red",
+        "gender": "Women",
+        "quantity": 2
+    },
+    {
+        "id": 11,
+        "imageURL": "./icons/grey-round-men.png",
+        "name": "Grey Round",
+        "type": "Basic",
+        "price": 300,
+        "currency": "INR",
+        "color": "Grey",
+        "gender": "Men",
+        "quantity": 1
+    },
+    {
+        "id": 12,
+        "imageURL": "./icons/purple-hoodie-women.png",
+        "name": "Purple Hoodie",
+        "type": "Hoodie",
+        "price": 500,
+        "currency": "INR",
+        "color": "Purple",
+        "gender": "Women",
+        "quantity": 3
+    },
+    {
+        "id": 13,
+        "imageURL": "./icons/grey-round-women.png",
+        "name": "Grey Round",
+        "type": "Basic",
+        "price": 300,
+        "currency": "INR",
+        "color": "Grey",
+        "gender": "Women",
+        "quantity": 1
+    },
+    {
+        "id": 14,
+        "imageURL": "./icons/white-round-women.png",
+        "name": "White Round",
+        "type": "Basic",
+        "price": 300,
+        "currency": "INR",
+        "color": "White",
+        "gender": "Women",
+        "quantity": 0
+    },
+    {
+        "id": 15,
+        "imageURL": "./icons/black-round-men.png",
+        "name": "Black Round",
+        "type": "Basic",
+        "price": 300,
+        "currency": "INR",
+        "color": "Black",
+        "gender": "Men",
+        "quantity": 7
+    },
+    {
+        "id": 16,
+        "imageURL": "./icons/purple-round-men.png",
+        "name": "Purple Round",
+        "type": "Basic",
+        "price": 300,
+        "currency": "INR",
+        "color": "Purple",
+        "gender": "Men",
+        "quantity": 1
+    },
+    {
+        "id": 17,
+        "imageURL": "./icons/white-round-men.png",
+        "name": "White Round",
+        "type": "Basic",
+        "price": 300,
+        "currency": "INR",
+        "color": "White",
+        "gender": "Men",
+        "quantity": 2
+    },
+    {
+        "id": 18,
+        "imageURL": "./icons/blue-round-women.png",
+        "name": "Blue Round",
+        "type": "Basic",
+        "price": 300,
+        "currency": "INR",
+        "color": "Blue",
+        "gender": "Women",
+        "quantity": 3
+    },
+    {
+        "id": 19,
+        "imageURL": "./icons/yellow-hoodie-women.png",
+        "name": "Yellow Hoodie",
+        "type": "Hoodie",
+        "price": 500,
+        "currency": "INR",
+        "color": "Yellow",
+        "gender": "Women",
+        "quantity": 1
+    },
+    {
+        "id": 20,
+        "imageURL": "./icons/white-polo-women.png",
+        "name": "White Polo",
+        "type": "Polo",
+        "price": 350,
+        "currency": "INR",
+        "color": "White",
+        "gender": "Women",
+        "quantity": 4
+    },
+    {
+        "id": 21,
+        "imageURL": "./icons/red-polo-men.png",
+        "name": "Red Polo",
+        "type": "Polo",
+        "price": 250,
+        "currency": "INR",
+        "color": "Red",
+        "gender": "Men",
+        "quantity": 2
+    },
+    {
+        "id": 22,
+        "imageURL": "./icons/blue-hoodie-men.png",
+        "name": "Blue Hoodie",
+        "type": "Hoodie",
+        "price": 500,
+        "currency": "INR",
+        "color": "Blue",
+        "gender": "Men",
+        "quantity": 0
+    },
+    {
+        "id": 23,
+        "imageURL": "./icons/grey-hoodie-men.png",
+        "name": "Grey Hoodie",
+        "type": "Hoodie",
+        "price": 500,
+        "currency": "INR",
+        "color": "Grey",
+        "gender": "Men",
+        "quantity": 2
+    },
+    {
+        "id": 24,
+        "imageURL": "./icons/grey-polo-men.png",
+        "name": "Grey Polo",
+        "type": "Polo",
+        "price": 300,
+        "currency": "INR",
+        "color": "Grey",
+        "gender": "Men",
+        "quantity": 3
+    },
+    {
+        "id": 25,
+        "imageURL": "./icons/red-hoodie-men.png",
+        "name": "Red Hoodie",
+        "type": "Hoodie",
+        "price": 300,
+        "currency": "INR",
+        "color": "Red",
+        "gender": "Men",
+        "quantity": 0
+    },
+    {
+        "id": 26,
+        "imageURL": "./icons/white-polo-men.png",
+        "name": "White Polo",
+        "type": "Polo",
+        "price": 300,
+        "currency": "INR",
+        "color": "White",
+        "gender": "Men",
+        "quantity": 1
+    },
+    {
+        "id": 27,
+        "imageURL": "./icons/white-hoodie-women.png",
+        "name": "White Hoodie",
+        "type": "Hoodie",
+        "price": 500,
+        "currency": "INR",
+        "color": "White",
+        "gender": "Women",
+        "quantity": 3
+    },
+    {
+        "id": 28,
+        "imageURL": "./icons/grey-round-men.png",
+        "name": "Grey Round",
+        "type": "Basic",
+        "price": 300,
+        "currency": "INR",
+        "color": "Grey",
+        "gender": "Men",
+        "quantity": 0
+    },
+    {
+        "id": 29,
+        "imageURL": "./icons/black-round-women.png",
+        "name": "Black Round",
+        "type": "Basic",
+        "price": 300,
+        "currency": "INR",
+        "color": "Black",
+        "gender": "Women",
+        "quantity": 0
+    },
+    {
+        "id": 30,
+        "imageURL": "./icons/black-polo-women.png",
+        "name": "Black Polo",
+        "type": "Polo",
+        "price": 300,
+        "currency": "INR",
+        "color": "Black",
+        "gender": "Women",
+        "quantity": 4
+    }
+];
+selected = {};
+const colors = {};
+const types = {};
+const genders = {};
+
+// Function to fetch and display products from the JSON file
+async function displayProducts(products) {
+    try {
+        const productListing = document.querySelector('.product-listings');
+        productListing.innerHTML = '';
+
+        products.forEach((product) => {
+
+            const productCard = document.createElement('div');
+            productCard.classList.add('product-card');
+            productCard.classList.add('prod-cont2');
+            productCard.setAttribute('id', product.id);
+
+            const productImage = document.createElement('img');
+            productImage.src = product.imageURL;
+            productImage.alt = product.name;
+            productImage.classList.add("prod-img");
+
+            element = document.createElement('div');
+            element.appendChild(productImage);
+            element.classList.add('img-div');
+
+            const productName = document.createElement('h2');
+            productName.textContent = product.name;
+            cardPriceAdd = document.createElement('div');
+
+            const productPrice = document.createElement('div');
+            productPrice.textContent = `${'Rs'} ${product.price}`;
+            cardPriceAdd.appendChild(productPrice);
+            productPrice.classList.add('price-div');
+
+            //Add to cart
+            const addToCartButton = document.createElement('div');
+            addToCartButton.innerText = "Add to cart";
+            addToCartButton.classList.add('add-div');
+            addToCartButton.addEventListener("click", () => {
+                addToCart(product.id);
+            });
+            cardPriceAdd.appendChild(addToCartButton);
+            cardPriceAdd.classList.add('price-addToCart-div');
+
+            productCard.appendChild(element);
+            productCard.appendChild(cardPriceAdd);
+
+            productListing.appendChild(productCard);
+        });
+    } catch (error) {
+        console.error('Error fetching or displaying products:', error);
+    }
+}
+
+// Call the function to display products when the page loads
+window.addEventListener('load', () => {
+    displayProducts(catalogue);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const selectFilterBtn = document.getElementById('filter-icon-sm')
+    const filterModal = document.getElementById('sm-filter')
+
+    selectFilterBtn.addEventListener("click", () => {
+        filterModal.classList.remove('invisible')
+    });
+
+
+});
+
+
+const updateCartCount = () => {
+    var cartCount = document.getElementById("cart-count");
+    const count = Object.values(selected).reduce((accumulator, value) => {
+        return accumulator + value;
+    }, 0);
+    if (count == 0) {
+        cartCount.innerHTML = "";
+    } else {
+        cartCount.innerHTML = count;
+    }
+}
+
+const replaceCountByAdd = (id) => {
+    var card = document.getElementById(id);
+    var cardPriceAdd = card.getElementsByClassName("price-addToCart-div")[0];
+    var addBtn = card.getElementsByClassName("add-div")[0];
+    addBtn.remove();
+    const addToCartButton = document.createElement('div');
+    addToCartButton.innerText = "Add to cart";
+    addToCartButton.classList.add('add-div');
+    addToCartButton.addEventListener("click", () => {
+        addToCart(id);
+    });
+    cardPriceAdd.appendChild(addToCartButton);
+}
+
+
+const replaceAddByCount = (id, count) => {
+    var card = document.getElementById(id);
+    var addBtn = card.getElementsByClassName("add-div")[0];
+    addBtn.remove();
+
+    const addToCartButton = document.createElement('div');
+    const addToCartIco = document.createElement('div');
+    addToCartIco.innerHTML = "+";
+    addToCartIco.addEventListener("click", () => {
+        addToCart(id);
+    });
+
+    const removeFromCartIco = document.createElement('div');
+    removeFromCartIco.innerHTML = "-";
+    removeFromCartIco.addEventListener("click", () => {
+        removeFromCart(id);
+    });
+
+    const countIco = document.createElement('div');
+    countIco.innerHTML = selected[id];
+
+
+    var cardPriceAdd = card.getElementsByClassName("price-addToCart-div")[0];
+    addToCartButton.classList.add('add-div');
+    addToCartIco.classList.add('add-ico');
+    removeFromCartIco.classList.add('rem-ico');
+    countIco.classList.add('count-ico');
+
+    addToCartButton.appendChild(removeFromCartIco);
+    addToCartButton.appendChild(countIco);
+    addToCartButton.appendChild(addToCartIco);
+
+    cardPriceAdd.appendChild(addToCartButton);
+    cardPriceAdd.classList.add('price-addToCart-div');
+}
+
+const addToCart = (id) => {
+    if (id in selected) {
+        selected[id] = selected[id] + 1;
+    }
+    else {
+        selected[id] = 1
+    }
+    replaceAddByCount(id, selected[id]);
+    updateCartCount();
+}
+
+const removeFromCart = (id) => {
+    if (id in selected) {
+        selected[id] -= 1;
+    }
+    if (selected[id] == 0) {
+        delete selected.id;
+        replaceCountByAdd(id);
+    } else {
+        replaceAddByCount(id, selected[id]);
+    }
+    updateCartCount();
+}
+
+
+const handleColorFilter = (products) => {
+    if (allFalse(colors)) {
+        return products;
+    }
+    return products.filter((obj) => {
+        if (obj.color.toLowerCase() in colors) {
+            return colors[obj.color.toLowerCase()];
+        } else {
+            return false;
+        }
+    })
+}
+
+
+const handleGenderFilter = (products) => {
+    if (allFalse(genders)) {
+        return products;
+    }
+    return products.filter((obj) => {
+        if (obj.gender.toLowerCase() in genders) {
+            return genders[obj.gender.toLowerCase()];
+        } else {
+            return false;
+        }
+    })
+}
+
+
+
+const handlePriceFilter = (products) => {
+    // Get references to the price range checkboxes
+    const price1Checkbox = document.getElementById("price1");
+    const price2Checkbox = document.getElementById("price2");
+    const price3Checkbox = document.getElementById("price3");
+
+    // Initialize arrays to store filtered products
+    const filteredProducts = [];
+
+    // Check the state of each checkbox and filter products accordingly
+    if (price1Checkbox.checked) {
+        filteredProducts.push(...products.filter((product) => product.price <= 250));
+    }
+    if (price2Checkbox.checked) {
+        filteredProducts.push(
+            ...products.filter((product) => product.price > 250 && product.price <= 450)
+        );
+    }
+    if (price3Checkbox.checked) {
+        filteredProducts.push(...products.filter((product) => product.price > 450));
+    }
+
+    // If no checkboxes are checked, return all products
+    if (!(price1Checkbox.checked || price2Checkbox.checked || price3Checkbox.checked)) {
+        return products;
+    }
+
+    // Remove duplicates from the filtered products array
+    const uniqueFilteredProducts = Array.from(new Set(filteredProducts));
+
+    return uniqueFilteredProducts;
+};
+
+
+const handleTypeFilter = (products) => {
+    if (allFalse(types)) {
+        return products;
+    }
+    return products.filter((obj) => {
+        if (obj.type.toLowerCase() in types) {
+            return types[obj.type.toLowerCase()];
+        } else {
+            return false;
+        }
+    })
+}
+
+const allFalse = (obj) => {
+    noFilter = true;
+    for (let i in Object.values(obj)) {
+        noFilter = noFilter && !Object.values(obj)[i];
+    }
+    return noFilter;
+}
+
+const handleFilters = (e, filter) => {
+    products = catalogue;
+    if (filter === 'color') {
+        colors[e.id] = e.checked;
+    }
+    if (filter === 'gender') {
+        genders[e.id] = e.checked;
+    }
+    if (filter === 'type') {
+        types[e.id] = e.checked;
+    }
+
+    checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    noFilter = true;
+    for (let i in checkboxes) {
+        noFilter = noFilter && !checkboxes[i].checked;
+    }
+
+    // Handle the price filter
+    products = handleColorFilter(products);
+    products = handleGenderFilter(products);
+    products = handlePriceFilter(products);
+    products = handleTypeFilter(products);
+
+    displayProducts(products);
+    return products;
+};
+
+
